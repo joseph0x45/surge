b546c59c5aa7966a44040aa7333661e166352623
joseph0x45 <zozozozeph@gmail.com>
2026-02-28 08:57:39 +0000

    feat: generate service file

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"fmt"
 	"os"
+
+	"github.com/joseph0x45/goutils"
 )
 
 func printUsage() {
@@ -20,6 +22,9 @@ func DispatchCommands(args []string) {
 		os.Exit(0)
 	case "create-user":
 		os.Exit(createUser(args[2:]))
+	case "service-file":
+		goutils.GenerateServiceFile("Surge, simple sitting time tracker")
+    os.Exit(0)
 	default:
 		fmt.Fprintf(os.Stderr, "Unrecognized command '%s'\n", cmd)
 		printUsage()
